a69fdf8116e61203a1b047f25aaf290e3065ee1f
Mark Hendrix Pascua <74813357+Kagami04@users.noreply.github.com>
2023-08-29 23:14:44 +0800

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import os
+
 
 # Face detection is done
 def faceDetection(test_img):
@@ -11,42 +11,13 @@ def faceDetection(test_img):
     return faces, grayscale_image
 
 
-# Labels for training data has been created
-
-def labels_for_training_data(directory):
-    faces = []
-    faceID = []
-
-    for path, subdirnames, filenames in os.walk(directory):
-        for filename in filenames:
-            if filename.startswith("."):
-                print("skipping system file")
-                continue
-            id = os.path.basename(path)
-            img_path = os.path.join(path, filename)
-            print("img_path", img_path)
-            print("id: ", id)
-            test_img = cv2.imread(img_path)
-            if test_img is None:
-                print("Not Loaded Properly")
-                continue
-
-            faces_rect, gray_img = faceDetection(test_img)
-            if len(faces_rect) != 1:
-                continue
-            (x, y, w, h) = faces_rect[0]
-            roi_gray = gray_img[y:y + w, x:x + h]
-            faces.append(roi_gray)
-            faceID.append(int(id))
-    return faces,faceID
-
 
 # Here training Classifier is called
-def train_classifier(face,faceID):
-    face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-    face_recognizer.train(face, np.array(faceID))
-    return face_recognizer
 
+def train_classifier(faces,ids):
+    recognizer = cv2.face.LBPHFaceRecognizer.create(radius=1, neighbors=8, grid_x=8, grid_y=8)
+    recognizer.train(faces, np.array(ids))
+    return recognizer
 
 # Drawing a Rectangle on the Face Function
 def draw_rect(test_img, face):
@@ -57,4 +28,5 @@ def draw_rect(test_img, face):
 # Putting text on images
 def put_text_with_background(test_img, text, x, y):
     cv2.putText(test_img, text, (x, y), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 2)
+
 
